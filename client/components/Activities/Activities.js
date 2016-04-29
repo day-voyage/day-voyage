da3cd9195bab0f2190ac73e2ddea7477449071ec
Vivian Chen <vivian.vc.chen@gmail.com>
2016-04-29 16:35:49 -0700

    (buildplanner) initial commit

--- client/components/Activities/Activities.js
+++ client/components/Activities/Activities.js
@@ -2,11 +2,19 @@ var ReactRouter = window.ReactRouter;
 let Link = ReactRouter.Link;
 
 class Activities extends React.Component {
+
   render() {
     return (
       <div className="container">
+      <div className="row">
+      <div className="col-md-4">
         <h1>Activities Page</h1>
         <button><Link to="/confirmation">go to confirmation page</Link></button>
+      </div>
+      <div className="col-md-4">
+        Builder Component
+      </div>
+      </div>
       </div>
     );
   }
