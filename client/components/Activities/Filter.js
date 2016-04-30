266a4709c67e09a9d5e7fc9b9007beee9732b1e9
Tor Sinclair <tor@romanesco.se>
2016-04-29 18:27:24 -0700

    Add state test properties on filter

--- client/components/Activities/Filter.js
+++ client/components/Activities/Filter.js
@@ -2,10 +2,25 @@ var ReactRouter = window.ReactRouter;
 let Link = ReactRouter.Link;
 
 class Filter extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      txt: 'this is the state txt'
+    }
+  }
+
+  update(event) {
+    this.setState({
+      txt: e.target.value
+    })
+  }
+
   render() {
     return (
       <div className="col-md-4">
-        Filter is here
+        <input type="text"
+          onChange={this.update.bind(this)} />
+          <h1>{this.state.txt}</h1>
       </div>
     );
   }
