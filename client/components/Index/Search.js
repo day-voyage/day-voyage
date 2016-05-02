a3d37946f9eb15c3a5a6b3b985c194d41c42519e
Steven Tran <steventran06@gmail.com>
2016-05-02 14:36:46 -0700

    (feat) Add helper components and implement db search

--- client/components/Index/Search.js
+++ client/components/Index/Search.js
@@ -5,34 +5,53 @@ export default class Search extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      value: ""
+      category: "",
+      city: "San Francisco, CA"
     };
   }
 
 
   searchYelp(event) {
     event.preventDefault();
+    console.log(this.state.city);
+    console.log(this.state.category);
 
-    fetch(`/api/yelpSearch?city=${this.state.value}`, {
+    fetch(`/api/yelpSearch?city=${this.state.city}&category=${this.state.category}`, {
       method: 'GET'
-    }).then((results) => results.json()).then((data) => console.log(data))
+    })
+    .then((results) => results.json()).then((data) => 
+      console.log("yelp data:", data));
+      fetch('http://localhost:3000/v1/activities', {
+        method: 'GET'
+      })
+      .then((dbResults) => dbResults.json()).then((dbData) => console.log("db Data:", dbData.data))
     .catch(e => console.log(e));
   }
 
-  handleChange(event) {
-    this.setState({value: event.target.value});
+  handleCategory(event) {
+    this.setState({category: event.target.value});
+  }
+
+  handleCity(event) {
+    this.setState({city: event.target.value});
   }
 
   render() {
     return (
       <div className="col-sm-12">
         <form className="commentForm" onSubmit={this.searchYelp.bind(this)}>
-          Search:
+          Search Category:
           <input
             type="text"
             value={this.state.value}
             placeholder="Category"
-            onChange={this.handleChange.bind(this)} />
+            onChange={this.handleCategory.bind(this)} />
+          Select City:
+          <select onChange={this.handleCity.bind(this)}>
+            <option value="San Francisco, CA" >San Francisco</option>
+            <option value="Oakland, CA" >Oakland</option>
+            <option value="San Jose, CA" >San Jose</option>
+          </select>
           <input type="submit" value="Post" />
         </form>
       </div>
