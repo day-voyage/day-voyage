98b54556824482fa36be428a9a3231e5b966d7a4
Steven Tran <steventran06@gmail.com>
2016-05-05 15:14:19 -0700

    (feat) Add modal to each activity with map

--- client/components/Activities/ActivityItem.js
+++ client/components/Activities/ActivityItem.js
@@ -1,16 +1,47 @@
-import React, { Component, PropTypes } from 'react'
+import React, { Component, PropTypes } from 'react';
+import Modal from 'react-modal';
+import Maps from '../Helpers/Maps';
 
 export default class ActivityItem extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      modalOpen: false
+    };
+  }
+
+  openModal() {
+    this.setState({
+      modalOpen: true
+    });
+  }
+
+  closeModal() {
+    this.setState({
+      modalOpen: false
+    });
+  }
+
   render() {
-    const { activity } = this.props
+    const { activity } = this.props;
 
     return (
       <div
         style={{ marginBottom: 20,
                 borderStyle: "solid",
                 borderWidth: "2px" }}>
-        <div> {activity.title} - {activity.desc} {activity.location } </div>
-
+        <div> {activity.title} - {activity.desc} {activity.neighborhood } <img src='../../assets/open.png' onClick={this.openModal.bind(this)} /></div>
+        <Modal
+          isOpen={this.state.modalOpen}
+          style={customStyles} >
+          <div className="container">
+            <div className="row">
+              <img src='../../assets/close.png' onClick={this.closeModal.bind(this)} />
+              <div> {activity.title} - {activity.neighborhood }</div>
+              <Maps size="small" lat={activity.lat} long={activity.long} title={activity.title} />
+            </div>
+          </div>
+        </Modal>
         <button
           onClick={this.props.onAddToBuilderClicked}
           disabled={activity.added ? 'disabled' : ''}>
@@ -31,3 +62,14 @@ ActivityItem.propTypes = {
   }).isRequired,
   onAddToBuilderClicked: PropTypes.func.isRequired
 }
+
+const customStyles = {
+  content : {
+    top                   : '50%',
+    left                  : '50%',
+    right                 : 'auto',
+    bottom                : 'auto',
+    marginRight           : '-50%',
+    transform             : 'translate(-50%, -50%)'
+  }
+};
