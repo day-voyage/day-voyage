0366c834b533ee415060799b646be3c91266ebe3
Vivian Chen <vivian.vc.chen@gmail.com>
2016-05-07 14:56:22 -0700

    add material-ui

---
+++ client/components/User/UserProfile.js
@@ -0,0 +1,17 @@
+import React, { Component, PropTypes } from 'react'
+
+// username - cannot update
+// first name
+// last name
+// email
+
+export default class UserProfile extends Component {
+  render() {
+
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
