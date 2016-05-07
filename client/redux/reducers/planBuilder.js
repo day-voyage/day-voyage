91db72906e7b5f25b912911dabda45bc5ef6e7d3
Vivian Chen <vivian.vc.chen@gmail.com>
2016-05-06 19:32:40 -0700

    reorder activities up/down

--- client/redux/reducers/planBuilder.js
+++ client/redux/reducers/planBuilder.js
@@ -1,8 +1,8 @@
 import {
   ADD_TO_BUILDER,
   DELETE_FROM_BUILDER,
-  CONFIRM_REQUEST,
-  CONFIRM_FAILURE
+  REORDER_UP,
+  REORDER_DOWN
 } from '../constants/ActionTypes'
 
 export function planBuilder(state = [], action) {
@@ -16,6 +16,26 @@ export function planBuilder(state = [], action) {
       var activityIndex = state.indexOf(action.activity);
       newState.splice(activityIndex, 1);
       return newState;
+    case REORDER_UP:
+      if (action.activityIndex === 0) {
+        return state;
+      } else {
+        var newState = state.slice();
+        var index = action.activityIndex;
+        var newIndex = action.activityIndex - 1;
+        newState[index] = newState.splice(newIndex, 1, newState[index])[0];
+        return newState;
+      }
+    case REORDER_DOWN:
+      if (action.activityIndex === state.length - 1) {
+        return state;
+      } else {
+        var newState = state.slice();
+        var index = action.activityIndex;
+        var newIndex = action.activityIndex + 1;
+        newState[index] = newState.splice(newIndex, 1, newState[index])[0];
+        return newState;
+      }
     default:
       return state
   }
