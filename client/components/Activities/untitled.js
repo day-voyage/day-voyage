0f81df8a56b02cc9e13b39f67bea340b1019a15b
Vivian Chen <vivian.vc.chen@gmail.com>
2016-05-07 18:26:04 -0700

    delete files

--- client/components/Activities/untitled.js
+++
@@ -1,7 +0,0 @@
-            <Geosuggest
-              className="geosuggest"
-              placeholder="Start typing!"
-              initialValue="San Francisco"
-              onSuggestSelect={this.onSuggestSelect}
-              location={new google.maps.LatLng(53.558572, 9.9278215)}
-              radius="20" />
