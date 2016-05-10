5b91db9ad2b1cd82a412ba54b5eae25b8c7a0058
Tor Sinclair <tor@romanesco.se>
2016-05-10 16:28:45 -0700

    remove incorrect state value in Activities view

--- src/views/ActivitiesView.js
+++ src/views/ActivitiesView.js
@@ -17,7 +17,6 @@ export class ActivitiesView extends React.Component {
     this.state = {
       snackbar: false,
       message: '',
-      value: 'a'
       minPrice: 0,
       maxPrice: 100
     };
