9eba7ea5018baa4cc92ec56eea4b75443b075094
Tor Sinclair <tor@romanesco.se>
2016-05-10 16:17:22 -0700

    refactor filter slider

--- src/containers/Filter.js
+++ src/containers/Filter.js
@@ -4,18 +4,40 @@ import Drawer from 'material-ui/Drawer';
 import MenuItem from 'material-ui/MenuItem';
 import Slider from 'material-ui/Slider';
 
+import Checkbox from 'material-ui/Checkbox';
+import ActionFavorite from 'material-ui/svg-icons/action/favorite';
+import ActionFavoriteBorder from 'material-ui/svg-icons/action/favorite-border';
+
+
 export default class FilterContainer extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      drawerOpen: false
+      drawerOpen: false,
+      priceSlider: this.props.maxPrice / 2,
+      cities: [
+        {location: 'Downtown',
+        visible: true},
+        {location: 'The Mission',
+        visible: true},
+        {location: 'Fillmore District',
+        visible: true},
+        {location: 'Hayes Valley',
+        visible: true},
+        {location: 'Marina District',
+        visible: true},
+        {location: 'Presidio Heights',
+        visible: true}
+      ],
+      checkBox: {
+        marginBottom: 16
+      }
     };
   }
 
   toggleDrawer() {
     this.setState({
-      drawerOpen: !this.state.drawerOpen,
-      priceSlider: this.props.maxPrice / 2
+      drawerOpen: !this.state.drawerOpen
     });
   }
 
@@ -23,18 +45,31 @@ export default class FilterContainer extends Component {
     this.setState({priceSlider: value});
     console.log(this.state.priceSlider)
   }
-  
+
+  handleCity(event, value) {
+    console.log('event ', event.target.value)
+    console.log('value ', event.target)
+  }
+
   render() {
+    
+    var cityOptions = (this.state.cities.map((city) => {
+              return (
+                <Checkbox index={city.id}
+                  label={city.location}
+                  defaultChecked={true}
+                  style={this.state.checkBox.marginBottom}
+                  onCheck={this.handleCity.bind(this)} />
+              )
+          }))
+
     return (
       <div>
         <FlatButton label="Filter" onClick={this.toggleDrawer.bind(this)} />
         <Drawer open={this.state.drawerOpen}>
           <FlatButton label="Close Filter" onClick={this.toggleDrawer.bind(this)} />
-          <MenuItem>Menu Item</MenuItem>
-          <MenuItem>Menu Item 2</MenuItem>
-        
           <div>
-          <MenuItem>Budget</MenuItem>
+          <MenuItem>Budget ${this.state.priceSlider}</MenuItem>
           <Slider
             min={this.props.minPrice}
             max={this.props.maxPrice}
@@ -42,6 +77,14 @@ export default class FilterContainer extends Component {
             defaultValue={this.props.maxPrice / 2}
             onChange={this.handleSlider.bind(this)}/>
           </div>
+
+
+
+            <div>
+            <MenuItem>Neighborhood</MenuItem>
+              {cityOptions}
+            </div>
+        
 
         </Drawer>
       </div>
