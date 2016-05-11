600268733ef7f6150265770459fa0ace0ef1d2a5
Steven Tran <steventran06@gmail.com>
2016-05-11 15:29:41 -0700

    (fix) remove extra styles

--- src/containers/ActivitiesContainer.js
+++ src/containers/ActivitiesContainer.js
@@ -47,17 +47,3 @@ export default connect(
   mapStateToProps,
   { addToBuilder }
 )(ActivitiesContainer)
-
-const styles = {
-  root: {
-    display: 'flex',
-    flexWrap: 'wrap',
-    justifyContent: 'space-around',
-  },
-  gridList: {
-    width: 500,
-    height: 500,
-    overflowY: 'auto',
-    marginBottom: 24,
-  },
-};
