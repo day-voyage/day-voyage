479bbb051a9ab0d4887b7c6ea6364faac1891e8e
Vivian Chen <vivian.vc.chen@gmail.com>
2016-05-11 16:08:33 -0700

    fix syntax error

--- src/containers/ActivitiesContainer.js
+++ src/containers/ActivitiesContainer.js
@@ -10,21 +10,28 @@ class ActivitiesContainer extends Component {
   render() {
     const { activities } = this.props;
     const hasActivities = activities.length > 0;
-    
+    const nodes = !hasActivities ?
+      <em>0 search results</em> :
+      <div>
+        <div>
+        {activities.map((activity, index) => {
+          if (activity.visible) {
+          return <ActivityItem
+            key={index}
+            activity={activity}
+            openSnackbar={this.props.openSnackbar}
+            onAddToBuilderClicked={() => {
+              this.props.addToBuilder(activity) }}/>
+          }
+        }
+      )}
+        </div>
+      </div>
+
     return (
       <Card>
         <h3 style={{marginLeft: 15}}>Activities</h3>
-        {!hasActivities ? <em>0 search results</em> :
-          activities.map((activity, index) =>
-            if (activity.visible) {
-              <ActivityItem
-                key={index}
-                activity={activity}
-                openSnackbar={this.props.openSnackbar}
-                onAddToBuilderClicked={() => {
-                  this.props.addToBuilder(activity) }}/>
-            }
-        )}
+        {nodes}
       </Card>
     )
   }
