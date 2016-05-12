5632c9f45b13643d3518c034fd0caefe0da5df04
Steven Tran <steventran06@gmail.com>
2016-05-12 16:08:19 -0700

    (fix) add auth restriction to confirm

--- src/containers/FilterContainer.js
+++ src/containers/FilterContainer.js
@@ -1,8 +1,6 @@
 
 import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux'
-
-
 import FlatButton from 'material-ui/FlatButton';
 import Drawer from 'material-ui/Drawer';
 import MenuItem from 'material-ui/MenuItem';
@@ -10,10 +8,7 @@ import Slider from 'material-ui/Slider';
 import Checkbox from 'material-ui/Checkbox';
 import ActionFavorite from 'material-ui/svg-icons/action/favorite';
 import ActionFavoriteBorder from 'material-ui/svg-icons/action/favorite-border';
-
-import {
-  checkArea,
-} from '../actions'
+import { checkArea } from '../actions';
 
 export default class FilterContainer extends Component {
   constructor(props) {
@@ -22,10 +17,7 @@ export default class FilterContainer extends Component {
       drawerOpen: false,
       priceSlider: this.props.maxPrice / 2,
       neighborhood: [],
-      cuisines: [],
-      checkBox: {
-        marginBottom: 16
-      }
+      cuisines: []
     };
   }
 
@@ -37,44 +29,40 @@ export default class FilterContainer extends Component {
 
   handleSlider(event, value) {
     this.setState({priceSlider: value});
-    console.log(this.state.priceSlider)
+    console.log(this.state.priceSlider);
   }
 
   componentWillMount() {
     const { activities } = this.props;
 
-    var areas_id = []
-    var areasArray = []    
-    var cuisineArr = []
-    var cuisines_id = []
+    var areas_id = [];
+    var areasArray = [];    
+    var cuisineArr = [];
+    var cuisines_id = [];
 
     activities.forEach((activity) => {
       for (var i = 0; i < activity.neighborhood.length; i++){
         if (areas_id.indexOf(activity.neighborhood[i].toUpperCase()) === -1) {
-          areas_id.push(activity.neighborhood[i].toUpperCase())
-          areasArray.push({location: activity.neighborhood[i].toUpperCase(), visible: true})
+          areas_id.push(activity.neighborhood[i].toUpperCase());
+          areasArray.push({location: activity.neighborhood[i].toUpperCase(), visible: true});
         }
       }
       for (var i = 0; i < activity.category.length; i++) {
         if (cuisines_id.indexOf(activity.category[i].toUpperCase()) === -1) {
-          cuisines_id.push(activity.category[i].toUpperCase())
-          cuisineArr.push({type: activity.category[i].toUpperCase(), visible: true}) 
+          cuisines_id.push(activity.category[i].toUpperCase());
+          cuisineArr.push({type: activity.category[i].toUpperCase(), visible: true}) ;
         }       
       }
     })
 
-    areasArray.sort((a, b) => a.location > b.location)
-    cuisineArr.sort((a, b) => a.type > b.type)
+    areasArray.sort((a, b) => a.location > b.location);
+    cuisineArr.sort((a, b) => a.type > b.type);
 
 
     this.setState({
-      neighborhood: areasArray
-    })
-
-    this.setState({
+      neighborhood: areasArray,
       cuisines: cuisineArr
-    })
-
+    });
   }
 
   handleCuisine(cuisine) {
@@ -85,12 +73,8 @@ export default class FilterContainer extends Component {
     for(var i = 0; i < cuisineArr.length; i++) {
       if(cuisineArr[i].type === cuisine && cuisineArr[i].visible === true) {
         cuisineArr[i].visible = false;
-
-        console.log('toggle to false')
-
       } else if (cuisineArr[i].type === cuisine && cuisineArr[i].visible === false) {
         cuisineArr[i].visible = true;
-        
       }
     }
   }
@@ -122,7 +106,7 @@ console.log('checkedAreas: ', checkedAreas);
           key={ index }
           label={area.location}
           defaultChecked={area.visible}
-          style={this.state.checkBox.marginBottom}
+          style={{marginBottom: 16}}
           onCheck={(e) => this.handleArea(area.location)} />
         )
     })
@@ -133,7 +117,7 @@ console.log('checkedAreas: ', checkedAreas);
           key={ index }
           label={cuisine.type}
           defaultChecked={true}
-          style={this.state.checkBox.marginBottom}
+          style={{marginBottom: 16}}
           onCheck={(e) => this.handleCuisine(cuisine.type)} />
       )
     })
