72ead9f846cbe6a3f9bd2167f0fc8828fd3205fb
Vivian Chen <vivian.vc.chen@gmail.com>
2016-05-14 15:32:53 -0700

    uncomment

--- src/components/Search.js
+++ src/components/Search.js
@@ -110,12 +110,11 @@ export class Search extends React.Component {
 
     // this.props.actions.updatePlan(2, {title: 'oh please'},[{activity_id: 3, title:'do it to meybabt'}, {activity_id: 4, title:'DA BOMB', desc: 'a man walks to a church, ha'}], result => console.log(result));
 
-    this.props.actions.getPlanWithActivities(2, (result) => console.log(result));
+    // this.props.actions.getPlanWithActivities(2, (result) => console.log(result));
 
     // this.props.actions.getAllPlans(plans => console.log(plans));
 
     // this.props.actions.queryTable('activities', {user_gen: true, city: 'San Francisco', 'title': 'heaven'}, (result) => console.log(result));
-// 
     // this.props.actions.deletePlan(4, (plan) => console.log(plan));
 
     // this.props.actions.getComments('user', 4, comment => console.log(comment));
