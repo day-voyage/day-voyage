788ac0ab0c3fd9ea2b0fde8959181c288c9a612f
Steven Tran <steventran06@gmail.com>
2016-05-15 14:00:45 -0700

    (fix) adjust styling for plan share

--- src/containers/PlanContainer.js
+++ src/containers/PlanContainer.js
@@ -6,12 +6,17 @@ import { Card, CardHeader } from 'material-ui/Card';
 
 export default class PlanContainer extends Component {
   render() {
+    const alphabetOrder = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
     return (
       <div className="container">
         <div className="row">
+          <div className="col-sm-12">
+            <h2 style={{marginLeft: 15, marginTop: 15}}>{this.props.plan ? this.props.plan.plan.title : "Plan"}</h2>
+          </div>
+        </div>
+        <div className="row">
           <div className="col-sm-6">
             <Card>
-              <h3 style={{marginLeft: 15, marginTop: 15}}>{this.props.plan ? this.props.plan.plan.title : "Plan"}</h3>
               <CardHeader
                 title="Plan Details"
                 subtitle={this.props.plan ? this.props.plan.plan.desc : null} />
@@ -19,13 +24,14 @@ export default class PlanContainer extends Component {
                 this.props.plan.activities.map((activity, index) =>
                   <PlanItem
                     key={index}
+                    order={alphabetOrder[index] + '.'}
                     activity={activity} />
                   ) : null
                 }
             </Card>
           </div>
           <div className="col-sm-6">
-          <div style={{marginTop: 20}}>
+          <div>
           </div>
             <PlanMaps size="small" activities={this.props.plan ? this.props.plan.activities : null}/>
           </div>
