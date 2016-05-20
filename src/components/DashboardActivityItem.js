a17098289eac98eb1e482c880626d0d00e35ef44
Vivian Chen <vivian.vc.chen@gmail.com>
2016-05-19 19:48:32 -0700

    dashboard styling

--- src/components/DashboardActivityItem.js
+++ src/components/DashboardActivityItem.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import FlatButton from 'material-ui/FlatButton';
-import {Card, CardHeader, CardText} from 'material-ui/Card';
+import {Card, CardHeader, CardText, CardActions} from 'material-ui/Card';
+import ContentAdd from 'material-ui/svg-icons/content/add';
+import IconButton from 'material-ui/IconButton';
+
 
 
 /**
@@ -40,7 +43,8 @@ export default class DashboardActivityItem extends Component {
     const { activity } = this.props;
     var title = activity.distance ? activity.title + ' - ' + activity.distance + " away" : activity.title;
     return (
-      <Card style={{padding: 15}}>
+      <Card className="item-card" style={{marginLeft: 10, marginRight:10, marginBottom: 10, cursor: 'pointer'}}>
+        <div className="card-content">
         <CardHeader
           title={title}
           subtitle={activity.neighborhood ? activity.neighborhood.join(', ') : ''}
@@ -48,11 +52,25 @@ export default class DashboardActivityItem extends Component {
           actAsExpander={true}
           showExpandableButton={true}
         />
-        <FlatButton
-          onClick={this.clickAddButton.bind(this)}
-          disabled={activity.added ? true : false}
-          label={activity.added ? 'Added' : 'Add to plan'} />
-        {this.state.descOpen ? <CardText><strong>Address:</strong><br />{activity.address}<br />{activity.city}, {activity.state}<br /><br />{activity.desc}</CardText> : null}
+        {this.state.descOpen ? 
+          <CardText>
+            <strong>Address:</strong><br />
+            {activity.address}<br />
+            {activity.city}, {activity.state}<br /><br />
+            {activity.desc}
+          </CardText> : null}
+        </div>
+        <div>
+        <CardActions>
+          <IconButton 
+            tooltip="Add to plan"
+            onClick={this.clickAddButton.bind(this)}
+            disabled={activity.added ? true : false}
+            label={activity.added ? 'Added' : ''}>
+            <ContentAdd />
+          </IconButton>
+        </CardActions>
+        </div>
       </Card>
     )
   }
